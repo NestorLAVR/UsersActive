f67632672f23261ba5b20ca3ba0666d843e1f9ca
Nestor <nestorlavrenov@gmail.com>
2020-09-29 17:28:59 +0300

    Last commit

--- UsersAuth/showusers/models.py
+++ UsersAuth/showusers/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 class DayVal(models.Model):
+    date = models.CharField(max_length=40, default='')
     DAU = models.IntegerField()
     MAU = models.IntegerField()
     date_day=models.CharField(max_length=40)
     date_month=models.CharField(max_length=50)
-    MAUgrowth=models.IntegerField()
-    DAUgrowth=models.IntegerField()
-    trMAU=models.CharField(max_length=10)
-    trDAU=models.CharField(max_length=10)
+    MAUgrowth=models.CharField(max_length=10)
+    DAUgrowth=models.CharField(max_length=10)
+    triaMAU=models.CharField(max_length=10)
+    triaDAU=models.CharField(max_length=10)
     colD=models.CharField(max_length=10)
     colM=models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.date
 
-    #"DAU": DAU, "MAU": MAU, "date_day": date_day, "date_month": date_month, "MAUgrowth": MAUgrowth, "DAUgrowth": DAUgrowth, "trMAU": tria_MAU, "trDAU": tria_DAU, "CD": colD, "CM": colM
+
 
 
--- UsersAuth/showusers/views.py
+++ UsersAuth/showusers/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
 from . functions import  UPorDOWN, DateRecieve, AbrDay, UsersAuth_DAY, UsersAuth_pDAY, UsersAuth_MON, UsersAuths_pMON, Date_Today
+from  .models import DayVal
+
 def showhowmany(request):
     date = DateRecieve()
-    DAU, sd = UsersAuth_DAY(date=date)
-    date_day = AbrDay(sd)
-    prevDAU = UsersAuth_pDAY(date=date)
-    MAU, sm, em = UsersAuth_MON(date=date)
-    date_month = AbrDay(sm) + " - " + AbrDay(em)
-    prevMAU = UsersAuths_pMON(date=date)
-    tria_DAU, DAUgrowth, colD = UPorDOWN(DAU, prevDAU)#изменения за прошлый день
-    tria_MAU, MAUgrowth, colM = UPorDOWN(MAU, prevMAU)#изменения за прошлый месяц
-    MAU=str(MAU)[:-3]+' '+ str(MAU)[-3:]
-    DAU = str(DAU)[:-3] + ' ' + str(DAU)[-3:]
-    date_today=Date_Today()
+    try:
+        DayValue= DayVal.objects.order_by('-id')[0]
+        daval=DayValue.date
+    except IndexError:
+        daval=''
+    if daval==str(date):
+        DAU =DayValue.DAU
+        MAU =DayValue.MAU
+        date_day=DayValue.date_day
+        date_month = DayValue.date_month
+        MAUgrowth = DayValue.MAUgrowth
+        DAUgrowth = DayValue.DAUgrowth
+        tria_MAU = DayValue.triaMAU
+        tria_DAU = DayValue.triaDAU
+        colD= DayValue.colD
+        colM= DayValue.colM
+
+    else:
+        DAU, sd = UsersAuth_DAY(date=date)
+        date_day = AbrDay(sd)
+        prevDAU = UsersAuth_pDAY(date=date)
+        MAU, sm, em = UsersAuth_MON(date=date)
+        date_month = AbrDay(sm) + " - " + AbrDay(em)
+        prevMAU = UsersAuths_pMON(date=date)
+        tria_DAU, DAUgrowth, colD = UPorDOWN(DAU, prevDAU)#изменения за прошлый день
+        tria_MAU, MAUgrowth, colM = UPorDOWN(MAU, prevMAU)#изменения за прошлый месяц
+        MAU=str(MAU)[:-3]+' '+ str(MAU)[-3:]
+        DAU = str(DAU)[:-3] + ' ' + str(DAU)[-3:]
+
+        f=DayVal(date=date, DAU=DAU, MAU=MAU, date_day=date_day, date_month=date_month, MAUgrowth=MAUgrowth, DAUgrowth=DAUgrowth,  colD=colD, colM=colM,  triaDAU=tria_DAU, triaMAU=tria_MAU,)
+        f.save()
+    date_today = Date_Today()
     date_today_year = date_today.year
-    date_today=AbrDay(date_today)+", "+str(date_today_year)
+    date_today = AbrDay(date_today) + ", " + str(date_today_year)
+
     context = {"DAU": DAU, "MAU": MAU , "date_day": date_day, "date_month": date_month, "MAUgrowth": MAUgrowth, "DAUgrowth":DAUgrowth, "trMAU":tria_MAU, "trDAU":tria_DAU, "CD": colD, "CM":colM,"date_today":date_today}
 
     return render(request, 'showusers/howmanyusers.html', context)
